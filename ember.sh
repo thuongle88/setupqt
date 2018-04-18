40c1fd625700e2830d0ecaedc35d03249e3ce038
thuongle88 <36128734+thuongle88@users.noreply.github.com>
2018-04-18 15:27:10 +0700

    Update ember.sh

--- ember.sh
+++ ember.sh
@@ -1,1 +1,25 @@
 
+#/bin/bash
+
+cd ~
+
+echo && echo && echo
+echo "*********************           Cai dat EmberCoin Wallet           ***********************"
+echo "*********************  I'm here to help you :) ***********************"
+echo && echo && echo
+
+
+sudo apt-get update
+sudo apt-get -y upgrade
+
+git clone https://github.com/ChartisProject/Ember
+cd Ember/src/leveldb && chmod 777 * && cd .. && make -f makefile.unix
+
+
+echo ""
+echo "**********************************************************************"
+echo ""
+echo "                      This should exit without errors                 "
+echo ""
+echo "**********************************************************************"
+echo ""
